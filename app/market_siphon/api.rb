9779f873dbe7d6580023ea63f1cb4709d04f1df1
Kyle S. Goodwin <goodwink@gmail.com>
2012-08-17 19:48:31 -0400

    Add 1x1 pixel web bug functionality and demo page to test.

--- app/market_siphon/api.rb
+++ app/market_siphon/api.rb
@@ -9,6 +9,12 @@ module MarketSiphon
     #rescue_from :all
     error_format :json
 
+    helpers do
+      def web_bug_gif
+        Base64.decode64("R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==")
+      end
+    end
+
     resource :tickets do
       desc 'Create a new session ticket using an account token and secret key.'
       params do
@@ -37,13 +43,14 @@ module MarketSiphon
     resource :referrals do
       desc 'Log a referral from an external site'
       params do
-        requires :source_url, type: URI, desc: 'URL of the page the referral is coming from'
-        requires :target_url, type: URI, desc: 'URL of the landing page on the referred site'
+        requires :source_url, type: String, desc: 'URL of the page the referral is coming from'
+        requires :target_url, type: String, desc: 'URL of the landing page on the referred site'
         requires :token, type: String, desc: 'account token'
       end
       post do
-        source = params[:source_url].host
-        target = params[:target_url].host
+        source = URI(params[:source_url]).host
+        target = URI(params[:target_url]).host
+        token = params[:token]
         account = Redis::HashKey.new('accounts:' + token)
 
         if account['target'] == target
@@ -66,6 +73,39 @@ module MarketSiphon
         else
           error!({error: {message: 'Invalid token or token is not associated with specified target url.'}}, 403)
         end
+      end
+
+      desc 'Log a referral from an external site via 1x1 pixel gif webbug'
+      params do
+        requires :source_url, type: String, desc: 'URL of the page the referral is coming from'
+        requires :token, type: String, desc: 'account token'
+      end
+      get :new do
+        content_type 'image/gif'
+
+        if env['HTTP_REFERER'] && token = params[:token]
+          source = URI(params[:source_url]).host
+          target = URI(env['HTTP_REFERER']).host
+          account = Redis::HashKey.new('accounts:' + token)
+
+          if account['target'] == target
+            ip = env['REMOTE_ADDR']
+
+            puts "Saving to #{'referrals:' + token + ':visitor:' + ip}"
+            referral = Redis::HashKey.new('referrals:' + token + ':visitor:' + ip)
+
+            if !referral[:converted]
+              referral['source'] = source
+              referral['target'] = target
+              referral['ip'] = ip
+
+              referrals_list = Redis::List.new('referrals:' + token)
+              referrals_list << ip
+            end
+          end
+        end
+          
+        web_bug_gif
       end
 
       desc 'Get a list of referrals for a single account'
@@ -94,12 +134,12 @@ module MarketSiphon
     resource :conversions do
       desc 'Log a conversion of a possible referral'
       params do
-        requires :conversion_url, type: URI, desc: 'url of the page that completes the conversion of the referral'
+        requires :conversion_url, type: String, desc: 'url of the page that completes the conversion of the referral'
         requires :token, type: String, desc: 'account token'
         optional :validation_token, type: String, desc: 'user-defined token used for validation and reconciliation of conversions'
       end
       post do
-        target = params[:conversion_url].host
+        target = URI(params[:conversion_url]).host
         account = Redis::HashKey.new('accounts:' + token)
 
         if account['target'] == target
